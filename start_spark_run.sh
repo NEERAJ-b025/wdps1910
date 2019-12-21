415c8d15e6e2adcef949010680a2e4a7d22bffe0
Neeraj-b025 <neeraj.sathian@gmail.com>
2019-12-21 02:10:20 +0100

    python 3.5 dependency

--- start_spark_run.sh
+++ start_spark_run.sh
@@ -3,7 +3,7 @@ INFILE=${2:-"hdfs:///user/wdps1910/sample.warc.gz"} #Give respective input file 
 OUTFILE=${3:-"result"} #Give respective output file in the HDFS
 SPARK_INSTANCE=${4:-"spark://node013.cm.cluster:7077"} #Give appropriate spark instance
 
-spark-submit \
+PYSPARK_PYTHON=python3.5 PYSPARK_DRIVER_PYTHON=python3.5 spark-submit \
 --master ${SPARK_INSTANCE} ${SCRIPT} ${INFILE} ${OUTFILE}
 
 hdfs dfs -cat ${OUTFILE}"/*" > ${OUTFILE}
